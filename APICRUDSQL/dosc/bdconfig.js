6b33ff0db4e428399d76cb3cf4362855aec5fa16
JgKill <80046756+JgKill@users.noreply.github.com>
2021-03-06 15:51:32 -0300

    Initial commit

---
+++ APICRUDSQL/dosc/bdconfig.js
@@ -0,0 +1,13 @@
+const config = {
+  user: "root",
+  password: "qwerty",
+  server: "localhots",
+  database: "DBTEST2",
+  option: {
+    trustedconnection: false,
+    enableArithAbort: true,
+    envrypt: false
+    //instancename:'<name>' en caso de que tenga una instancia
+  }
+};
+module.exports = config;
---
+++ APICRUDSQL/dosc/dbcategoria.js
@@ -0,0 +1,9 @@
+var config = require("./dbcopnfig"); // instanciamos el archivo dbconfig
+const sql = require("mysql"); //se necesirta paquete mssql
+
+async function getCategoria() {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
